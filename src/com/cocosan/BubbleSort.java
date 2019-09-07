22a3e9f6f0acd0be92ae7bff23e3342b0120beb5
cocowang <whz20024117@gmail.com>
2019-09-07 19:53:06 -0400

    Initial commit

---
+++ src/com/cocosan/BubbleSort.java
@@ -0,0 +1,38 @@
+package com.cocosan;
+
+public class BubbleSort {
+    public static void sort(Comparable[] a){
+        int N = a.length;
+        for(int i = 0; i < N-1; i++){
+            for (int j = 1; j < N-i; j++){
+                if (less(a[j],a[j-1])) exch(a, j, j-1);
+            }
+        }
+    }
+
+    public static void sort_sida(Comparable[] a){
+        int N = a.length;
+        boolean swap = true;
+        int k = 0;
+        while (swap){
+            swap = false;
+            for (int j = 0; j < N - k - 1; j++){
+                if (less(a[j+1], a[j])){
+                    exch(a, j+1, j);
+                    swap = true;
+                }
+             k++;
+            }
+        }
+    }
+
+    private static boolean less(Comparable v, Comparable w){
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j){
+        Comparable swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+}
---
+++ src/com/cocosan/Main.java
@@ -0,0 +1,37 @@
+package com.cocosan;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Integer[] a = {1,5,2,3,4};
+        //Integer[] a = get_ints();
+        long start = System.nanoTime();
+        BubbleSort.sort_sida(a);
+        long finish = System.nanoTime();
+        //for(int i: a) System.out.println(i);
+        System.out.println("Time used: "+(finish-start)*1e-9);
+        System.out.println(isSorted(a));
+    }
+
+    private static Integer[] get_ints(){
+        Random rd = new Random(100);
+        Integer[] arr = new Integer[10];
+        for (int i = 0; i < arr.length; i++){
+            arr[i] = rd.nextInt();
+        }
+        return arr;
+    }
+
+    private static boolean isSorted(Comparable[] a){
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i], a[i - 1])) return false;
+        }
+        return true;
+    }
+
+    private static boolean less(Comparable v, Comparable w){
+        return v.compareTo(w) < 0;
+    }
+}
